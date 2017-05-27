27e8f7eb5178ac0424439d157cdb6710413334b9
Yubisay <say.arts@gmail.comssh-keygen -t rsa -C say.arts@gmail.com>
2017-05-27 14:52:26 -0400

    fullscreen/off-canvas menu-

--- assets/app.js
+++ assets/app.js
@@ -1,32 +1,24 @@
-/* SEARCH*/
-$(document).foundation();
+//This is just to add/remove menu-open class on body using plain javascript (practice purpose). Jquery code to replace this is: 
 
-$(document).ready(function () {
-    var submitIcon = $('.searchbox-icon');
-    var inputBox = $('.searchbox-input');
-    var searchBox = $('.search-input-animated');
-    var isOpen = false;
-    submitIcon.click(function () {
-        if (isOpen == false) {
-            searchBox.addClass('searchbox-open');
-            inputBox.focus();
-            isOpen = true;
-        } else {
-            searchBox.removeClass('searchbox-open');
-            inputBox.focusout();
-            isOpen = false;
-        }
-    });
-    submitIcon.mouseup(function () {
-        return false;
-    });
-    searchBox.mouseup(function () {
-        return false;
-    });
-    $(document).mouseup(function () {
-        if (isOpen == true) {
-            $('.searchbox-icon').css('display', 'block');
-            submitIcon.click();
-        }
-    });
-});
+// $('.hamburger-menu, .link').click(function () {
+//  $('body').hasClass('menu-open') ? ($('body').removeClass('menu-open')) : ($('body').addClass('menu-open'));
+// });
+
+//However, don't forget to include jQuery for it to work.
+
+function toggleMenu() {
+    var body = document.body;
+        body.className.match(/(?:^|\s)menu-open(?!\S)/) ? body.className = body.className.replace( /(?:^|\s)menu-open(?!\S)/g , '' ) : body.className += "menu-open";
+}
+
+window.onload = function() {
+    var hamburger = document.getElementsByClassName('hamburger-menu')[0];
+    var menuLinks = document.getElementsByClassName('link');
+    
+    hamburger.addEventListener( 'click' , toggleMenu );
+    
+    for(var i = 0; i < menuLinks.length; ++i) {
+        var menuLink = menuLinks[i];
+        menuLink.addEventListener( 'click' , toggleMenu );
+    }
+}
